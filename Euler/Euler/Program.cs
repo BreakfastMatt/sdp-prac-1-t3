1cc71c434eca4341e0ac0fc5a7fd6b4e3e5b6fe0
Damon Wolff <G17W6029@ict.ru.ac.za>
2019-02-15 13:04:38 +0200

    Added Multiples of 3 and 5 solution

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -1,5 +1,14 @@
 ﻿using System;
 
+/*
+ * Authors:
+ *   
+ * Mak
+ * Damon 
+ * Breakfast boi
+ * 
+*/
+
 namespace Euler
 {
     class Program
@@ -7,6 +16,24 @@ namespace Euler
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            MultiplesOf3and5();
+            Console.ReadLine();
+        }
+
+        public static void MultiplesOf3and5()
+        {
+            int sum =0;
+            int current = 1;
+            while (current < 1000)
+            {
+                if(current%3 == 0 || current%5 == 0)
+                {
+                    sum = sum +current ; 
+                }
+                current++;
+            }
+
+            Console.WriteLine("The sum of multiples between 3 and 5 below 1000 is "+ Convert.ToString(sum) +"!");
         }
     }
 }
