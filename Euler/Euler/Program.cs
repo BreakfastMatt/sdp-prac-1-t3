7f4382ae10c3d8abdb183c8c2097cddac90cd618
Makungu Chansa <G17C9313@ict.ru.ac.za>
2019-02-15 13:04:46 +0200

    added solution to Sum Square Difference

--- Euler/Euler/Program.cs
+++ Euler/Euler/Program.cs
@@ -7,6 +7,23 @@ namespace Euler
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine(SumSquareDifference());
+        }
+
+        static int SumSquareDifference()
+        {
+            int SumSquare = 0;
+            int SquareSum = 0;
+          
+            for (int i = 1; i <= 100; i++)
+            {
+                SumSquare += i * i;
+                SquareSum += i;
+            }
+
+            SquareSum = SquareSum * SquareSum;
+
+            return SquareSum - SumSquare;
         }
     }
 }
